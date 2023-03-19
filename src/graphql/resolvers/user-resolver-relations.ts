20775f77dafa662ea0e74d50462e21cc6c88e956
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-03-19 22:16:28 +0100

    change query "registrations" on UserOutput to only return verified records

--- src/graphql/resolvers/user-resolver-relations.ts
+++ src/graphql/resolvers/user-resolver-relations.ts
@@ -4,19 +4,29 @@ import { RegistrationOutput } from "../outputs/registration-output";
 import { UserOutput } from "../outputs/user-output";
 import { GraphqlContext } from "../type-defs";
 
-@Resolver(of => UserOutput)
+@Resolver((of) => UserOutput)
 export class UserResolverRelations {
     @Authorized()
-    @FieldResolver(returns => String)
+    @FieldResolver((returns) => String)
     npub(@Root() user: UserOutput): string {
         return Nostr.Pubkey2nPub(user.pubkey);
     }
 
     @Authorized()
-    @FieldResolver(returns => [RegistrationOutput])
-    async registrations(@Root() user: UserOutput, @Ctx() context: GraphqlContext): Promise<RegistrationOutput[]> {
-        const dbRegistrations = context.db.registration.findMany({ where: { userId: user.id } });
+    @FieldResolver((returns) => [RegistrationOutput])
+    async registrations(
+        @Root() user: UserOutput,
+        @Ctx() context: GraphqlContext
+    ): Promise<RegistrationOutput[]> {
+        const dbRegistrations = context.db.registration.findMany({
+            where: {
+                userId: user.id,
+                verifiedAt: {
+                    not: null,
+                },
+            },
+        });
 
-        return (await dbRegistrations).sortBy(x => x.identifier);
+        return (await dbRegistrations).sortBy((x) => x.identifier);
     }
 }
