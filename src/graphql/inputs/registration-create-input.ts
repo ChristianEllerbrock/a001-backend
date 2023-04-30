1ef44afbcb346e076770e587edaec981dd754a58
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-04-30 22:48:45 +0200

    allow registering from all systemDomains

--- src/graphql/inputs/registration-create-input.ts
+++ src/graphql/inputs/registration-create-input.ts
@@ -1,9 +1,12 @@
-import { ArgsType, Field } from "type-graphql";
+import { ArgsType, Field, Int } from "type-graphql";
 
 @ArgsType()
 export class RegistrationCreateInput {
     @Field((type) => String)
     identifier!: string;
+
+    @Field((type) => Int)
+    systemDomainId!: number;
 
     @Field((type) => String)
     npub!: string;
