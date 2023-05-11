14e2f0246c695c18cdf7c8c309bad5f0ef85babb
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-05-11 20:43:40 +0200

    add mutations to handle add and delete on the resolver "registration-relay-resolver"

--- src/graphql/resolvers/registration-relay-resolver.ts
+++ src/graphql/resolvers/registration-relay-resolver.ts
@@ -1,9 +1,10 @@
-import { Args, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
+import { Arg, Args, Authorized, Ctx, Mutation, Resolver } from "type-graphql";
 import { UpdateRegistrationRelayInputArgs } from "../inputs/update-registration-relay-input";
 import { RegistrationOutput } from "../outputs/registration-output";
 import { GraphqlContext } from "../type-defs";
 import { HelperRegex } from "../../helpers/helper-regex";
 import { RegistrationRelayOutput } from "../outputs/registration-relay-output";
+import { Nip05CacheService } from "../../services/nip05-cache-service";
 
 @Resolver()
 export class RegistrationRelayResolver {
@@ -17,7 +18,9 @@ export class RegistrationRelayResolver {
             await context.db.registrationRelay.findUnique({
                 where: { id: args.registrationRelayId },
                 include: {
-                    registration: true,
+                    registration: {
+                        include: { systemDomain: true },
+                    },
                 },
             });
 
@@ -44,7 +47,90 @@ export class RegistrationRelayResolver {
                 },
             });
 
+        // Invalidate NIP-05 cache for this registration
+        Nip05CacheService.instance.invalidate(
+            dbRegistrationRelay.registration.identifier +
+                "@" +
+                dbRegistrationRelay.registration.systemDomain.name
+        );
+
         return updatedDbRegistrationRelay;
+    }
+
+    @Authorized()
+    @Mutation((returns) => Boolean)
+    async deleteRegistrationRelay(
+        @Ctx() context: GraphqlContext,
+        @Arg("registrationRelayId") registrationRelayId: string
+    ): Promise<boolean> {
+        const dbRegistrationRelay =
+            await context.db.registrationRelay.findUnique({
+                where: { id: registrationRelayId },
+                include: {
+                    registration: {
+                        include: { systemDomain: true },
+                    },
+                },
+            });
+
+        if (
+            !dbRegistrationRelay ||
+            context.user?.userId !== dbRegistrationRelay.registration.userId
+        ) {
+            throw new Error(
+                "Could not find the requested relay or unauthorized."
+            );
+        }
+
+        await context.db.registrationRelay.delete({
+            where: { id: registrationRelayId },
+        });
+
+        // Invalidate NIP-05 cache for this registration
+        Nip05CacheService.instance.invalidate(
+            dbRegistrationRelay.registration.identifier +
+                "@" +
+                dbRegistrationRelay.registration.systemDomain.name
+        );
+
+        return true;
+    }
+
+    @Authorized()
+    @Mutation((returns) => RegistrationRelayOutput)
+    async addRegistrationRelay(
+        @Ctx() context: GraphqlContext,
+        @Arg("registrationId") registrationId: string,
+        @Arg("relay", (type) => String, { nullable: true }) relay: string | null
+    ): Promise<RegistrationRelayOutput> {
+        const dbRegistration = await context.db.registration.findUnique({
+            where: { id: registrationId },
+            include: { systemDomain: true },
+        });
+
+        if (!dbRegistration || dbRegistration.userId !== context.user?.userId) {
+            throw new Error(
+                "Could not find the requested registration or unauthorized."
+            );
+        }
+
+        const cleanedRelay = relay
+            ? relay.trim().toLowerCase()
+            : "wss://changeme.com";
+
+        const dbRegistrationRelay = await context.db.registrationRelay.create({
+            data: {
+                registrationId: dbRegistration.id,
+                address: cleanedRelay,
+            },
+        });
+
+        // Invalidate NIP-05 cache for this registration
+        Nip05CacheService.instance.invalidate(
+            dbRegistration.identifier + "@" + dbRegistration.systemDomain.name
+        );
+
+        return dbRegistrationRelay;
     }
 }
 
