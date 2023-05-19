4f06a5aab84f0a478d2615bfa5c0d47d22c13d6a
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-05-19 13:12:39 +0200

    update seeding

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -3,10 +3,14 @@ import { seedUsers } from "./seed/seed-users";
 import { seedSystemBlockedIdentifier } from "./seed/seed-system-blocked-identifier";
 import { seedSystemConfig } from "./seed/seed-system-config";
 import { seedSystemDomain } from "./seed/seed-system-domain";
+import { seedJobState } from "./seed/seed-job-state";
+import { seedJobType } from "./seed/seed-job-type";
 
 const prisma = new PrismaClient();
 
 async function main() {
+    await seedJobState(prisma);
+    await seedJobType(prisma);
     await seedSystemConfig(prisma);
     await seedSystemDomain(prisma);
     await seedSystemBlockedIdentifier(prisma);
