5545e0f70c19d4b0afaef5fd0a54d0dec4d2c688
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-05-26 23:41:55 +0200

    fix seeding

--- src/prisma/seed/seed-users.ts
+++ src/prisma/seed/seed-users.ts
@@ -39,70 +39,70 @@ const seed = async function (prisma: PrismaClient) {
 
     // #region bot users
 
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "decfe634a6a6a6025fb59d4e139026381242b9ddad6b8d612d370c343942c005",
         "bot",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "c6d5eb25e5b352ba8e0e5bf5e70f79d6f18492d1fc294554a53996d4755221ef",
         "bot2",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "4b1ef958fe009df0c696b7443034c9d4f4e15b9948553e86693b43e689449961",
         "bot3",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "b96fc93681c73f2477a67bf462025d6b6f843db2aaea2b410f8e593316bfa92a",
         "bot4",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "50b61ee8f15860252a3835fd949a11b1ce5b01bb352d53b7e5437f130c178984",
         "bot5",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "f975acbafed19f190ec608c800dcd2acf4d2b376cdc144582cf51ca728a219c5",
         "bot6",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "d1f76005694e26ba955370ca74d86defe2862564c3eea79a7414d9a5fa30a9f4",
         "bot7",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "2b95e398f5c20509605639300c7a52252f77380e9d3268230bf0b49a277a0a87",
         "bot8",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "1e4ab9bf9395959dca9a52dfdfa83e38f74aa0153ece44163e3e4c71e9c81fcc",
         "bot9",
         1
     );
-    seedBot(
+    await seedBot(
         prisma,
         seedDate,
         "8377ae5e4c818aa4e429a08f009d64295d43057bd90a5325137051602a432ef7",
