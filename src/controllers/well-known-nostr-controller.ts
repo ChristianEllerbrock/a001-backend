6d00fae2ad34266d49650e239a83a81a94a1b0f5
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-11-04 23:33:48 +0100

    add lastLookupDate to EmailNostr

--- src/controllers/well-known-nostr-controller.ts
+++ src/controllers/well-known-nostr-controller.ts
@@ -86,6 +86,7 @@ export async function wellKnownNostrController(
                 },
                 data: {
                     lookups: { increment: 1 },
+                    lastLookupDate: new Date(),
                 },
             });
 
