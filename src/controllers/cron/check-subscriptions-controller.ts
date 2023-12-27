72488d5c46f090434f7ce64dec2b9d221c7f9d2a
Christian Ellerbrock <christian.ellerbrock@inbayern.net>
2023-12-27 22:30:02 +0100

    finalue controller "checkSubscription"

--- src/controllers/cron/check-subscriptions-controller.ts
+++ src/controllers/cron/check-subscriptions-controller.ts
@@ -3,6 +3,7 @@ import { EnvService } from "../../services/env-service";
 import { PrismaService } from "../../services/prisma-service";
 import { Nip05NostrService } from "../../services/nip05-nostr/nip05-nostr-service";
 import { sleep } from "../../helpers/sleep";
+import { DateTime } from "luxon";
 
 const log = function (text: string | object) {
     console.log(`CHECK SUBSCRIPTIONS - [controller] - ${JSON.stringify(text)}`);
@@ -35,8 +36,25 @@ const message_DowngradedToBasic =
     "Your paid subscription has expired and your account was downgraded to the free BASIC plan." +
     " You can upgrade your plan at any time on https://nip05.social";
 
+const message_14DaysLeft =
+    "You have 14 days left before your paid subscription expires and you will be downgraded to the free BASIC plan." +
+    " Log in to your account and prolong your subscription on https://nip05.social";
+
+const message_7DaysLeft =
+    "You have 7 days left before your paid subscription expires and you will be downgraded to the free BASIC plan." +
+    " Log in to your account and prolong your subscription on https://nip05.social";
+
+const message_3DaysLeft =
+    "You have 3 days left before your paid subscription expires and you will be downgraded to the free BASIC plan." +
+    " Log in to your account and prolong your subscription on https://nip05.social";
+
+const message_1DaysLeft =
+    "You have 1 day left before your paid subscription expires and you will be downgraded to the free BASIC plan." +
+    " Log in to your account and prolong your subscription on https://nip05.social";
+
 const runCheckSubscriptions = async function () {
     const now = new Date();
+    const nowDateTime = DateTime.fromJSDate(now);
 
     //
     // 1. Check for subscriptions that have expired
@@ -81,6 +99,149 @@ const runCheckSubscriptions = async function () {
             message_DowngradedToBasic
         );
         await sleep(1000); // Just to be sure to not stress the relays.
+    }
+
+    //
+    // 2. Check for subscriptions that will expire shortly
+    // => Notify the user if necessary.
+    //
+    const offset = 0.042;
+    const inDays14 = nowDateTime.plus({ days: 14 + offset });
+    const inDays7 = nowDateTime.plus({ days: 7 + offset });
+    const inDays3 = nowDateTime.plus({ days: 3 + offset });
+    const inDays1 = nowDateTime.plus({ days: 1 + offset });
+
+    const dbUserWithSubscriptionAboutToExpire =
+        await PrismaService.instance.db.user.findMany({
+            where: {
+                subscriptionEnd: {
+                    not: null,
+                    lte: inDays14.toJSDate(),
+                    gt: now,
+                },
+            },
+            include: {
+                userSubscriptions: { orderBy: { createdAt: "desc" }, take: 1 },
+            },
+        });
+
+    for (const user of dbUserWithSubscriptionAboutToExpire) {
+        if (user.userSubscriptions.length === 0) {
+            continue;
+        }
+
+        const latestUserSubscription = user.userSubscriptions[0];
+        console.log(latestUserSubscription);
+        if (
+            latestUserSubscription.newSubscriptionEnd?.getTime() !=
+                user.subscriptionEnd?.getTime() ||
+            latestUserSubscription.newSubscriptionEnd == null
+        ) {
+            continue; // Should not happen.
+        }
+
+        if (
+            latestUserSubscription.expirationReminder14 != null &&
+            latestUserSubscription.expirationReminder7 != null &&
+            latestUserSubscription.expirationReminder3 != null &&
+            latestUserSubscription.expirationReminder1 != null
+        ) {
+            continue; // All reminders were sent. Nothing to do here.
+        }
+
+        if (
+            latestUserSubscription.newSubscriptionEnd.getTime() <=
+                inDays14.toJSDate().getTime() &&
+            latestUserSubscription.newSubscriptionEnd.getTime() >
+                inDays7.toJSDate().getTime()
+        ) {
+            // Situation: Days14
+            if (latestUserSubscription.expirationReminder14 !== null) {
+                continue; // Already notified.
+            }
+
+            log(`Notify account '${user.pubkey}': 14 days remaining.`);
+            const relevantUserRelays = await determineRelevantRelays(user.id);
+            await Nip05NostrService.instance.sendDMFromBot(
+                user.pubkey,
+                relevantUserRelays,
+                message_14DaysLeft
+            );
+
+            await PrismaService.instance.db.userSubscription.update({
+                where: { id: latestUserSubscription.id },
+                data: { expirationReminder14: now },
+            });
+            await sleep(1000); // Just to be sure to not stress the relays.
+            continue;
+        } else if (
+            latestUserSubscription.newSubscriptionEnd.getTime() <=
+                inDays7.toJSDate().getTime() &&
+            latestUserSubscription.newSubscriptionEnd.getTime() >
+                inDays3.toJSDate().getTime()
+        ) {
+            // Situation: Days7
+            if (latestUserSubscription.expirationReminder7 !== null) {
+                continue; // Already notified.
+            }
+
+            log(`Notify account '${user.pubkey}': 7 days remaining.`);
+            const relevantUserRelays = await determineRelevantRelays(user.id);
+            await Nip05NostrService.instance.sendDMFromBot(
+                user.pubkey,
+                relevantUserRelays,
+                message_7DaysLeft
+            );
+            await PrismaService.instance.db.userSubscription.update({
+                where: { id: latestUserSubscription.id },
+                data: { expirationReminder7: now },
+            });
+            await sleep(1000); // Just to be sure to not stress the relays.
+            continue;
+        } else if (
+            latestUserSubscription.newSubscriptionEnd.getTime() <=
+                inDays3.toJSDate().getTime() &&
+            latestUserSubscription.newSubscriptionEnd.getTime() >
+                inDays1.toJSDate().getTime()
+        ) {
+            // Situation: Days3
+            if (latestUserSubscription.expirationReminder3 !== null) {
+                continue; // Already notified.
+            }
+
+            log(`Notify account '${user.pubkey}': 3 days remaining.`);
+            const relevantUserRelays = await determineRelevantRelays(user.id);
+            await Nip05NostrService.instance.sendDMFromBot(
+                user.pubkey,
+                relevantUserRelays,
+                message_3DaysLeft
+            );
+            await PrismaService.instance.db.userSubscription.update({
+                where: { id: latestUserSubscription.id },
+                data: { expirationReminder3: now },
+            });
+            await sleep(1000); // Just to be sure to not stress the relays.
+            continue;
+        } else {
+            // Situation: Day1
+            if (latestUserSubscription.expirationReminder1 !== null) {
+                continue; // Already notified.
+            }
+
+            log(`Notify account '${user.pubkey}': 1 day remaining.`);
+            const relevantUserRelays = await determineRelevantRelays(user.id);
+            await Nip05NostrService.instance.sendDMFromBot(
+                user.pubkey,
+                relevantUserRelays,
+                message_1DaysLeft
+            );
+            await PrismaService.instance.db.userSubscription.update({
+                where: { id: latestUserSubscription.id },
+                data: { expirationReminder1: now },
+            });
+            await sleep(1000); // Just to be sure to not stress the relays.
+            continue;
+        }
     }
 };
 
